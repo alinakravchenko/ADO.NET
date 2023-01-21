487147ace588f095c279eede47d720d675023b34
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2023-01-21 14:53:48 +0300

    Add project files.

---
+++ ADO_HM1/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace ADO_HM1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подключиться";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Отключиться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Показать все записи ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 196);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(193, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = " Отображение всех названий";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(173, 274);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Все цвета";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(229, 303);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(140, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Макс. калорийность";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(265, 274);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(128, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Мин. калорийность";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 363);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(193, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = " Средн. калорийность";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Фруктов:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Овощей";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 12;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(254, 67);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(274, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = " Количество овощей и фруктов заданного цвета;";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(254, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(254, 96);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(190, 23);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "калорийностью ниже указанной;";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(254, 125);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(190, 23);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "калорийностью выше указанной";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(252, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(358, 154);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(254, 180);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(545, 23);
+            this.button12.TabIndex = 19;
+            this.button12.Text = "овощи и фрукты с калорийностью в указанном диапазоне";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(12, 332);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(474, 25);
+            this.button13.TabIndex = 20;
+            this.button13.Text = " Показать все овощи и фрукты, у которых цвет желтый или красный.";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 415);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Общее кол-во";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
+
---
+++ ADO_HM1/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_HM1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = null;
+        SqlDataReader dr = null;
+        SqlCommand cmd = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Fruits_vegetables;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            conn.Open();
+            if (conn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Успешное подключение");
+            }
+            else
+            {
+                MessageBox.Show("Попробуйте ище раз");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Close();
+            if (conn.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("Вы отключены");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select * from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                textBox1.Text += dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2) + "\t" + dr.GetInt32(3) + "\r\n";
+
+
+            }
+            dr.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select Name from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                textBox1.Text += dr.GetString(0) + "\r\n";
+
+
+            }
+            dr.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select color from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                textBox1.Text += dr.GetString(0) + "\r\n";
+
+
+            }
+            dr.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select max(Calorie) from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader(); //выдает тут ошибку
+            while (dr.Read())
+            {
+                textBox1.Text += dr.GetInt32(0) + "\r\n";
+
+
+            }
+            dr.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select min(Calorie) from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader(); //выдает тут ошибку
+            while (dr.Read())
+            {
+                textBox1.Text += dr.GetInt32(0) + "\r\n";
+            }
+            dr.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select avg(Calorie) from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader(); //выдает тут ошибку, возможно ошибка в самих таблицах
+            while (dr.Read())
+            {
+                textBox1.Text += dr.GetInt32(0) + "\r\n";
+            }
+            dr.Close();
+        }
+        int countv = 0;
+        int countf = 0;
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Fruits_vegetables;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            conn.Open();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select type from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.GetString(0).ToLower() == "vegetable")
+                {
+                    countv++;
+                }
+                if (dr.GetString(0).ToLower() == "fruit")
+                {
+                    countf++;
+                }
+            }
+            label4.Text = countv.ToString();
+            label1.Text = countf.ToString();
+            dr.Close();
+        }
+
+
+        int countC;
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select color from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.GetString(0).ToLower() == textBox2.Text.ToLower())
+                {
+                    countC++;
+                }
+                else
+                {
+                    textBox1.Text = "Nothing";
+                }
+
+            }
+            textBox1.Text = countC.ToString();
+
+            dr.Close();
+        }
+
+
+        //BUTTON 10, BUTTON 11. НЕ РАБОТАЕТ
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select * from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.GetInt32(3) < int.Parse(textBox2.Text))
+                {
+                    textBox1.Text += dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2) + "\t" + dr.GetInt32(3) + "\r\n";
+                }
+                else
+                {
+                    textBox1.Text = "Nothing";
+                }
+
+            }
+            dr.Close();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select * from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.GetInt32(3) > int.Parse(textBox2.Text))
+                {
+                    textBox1.Text += dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2) + "\t" + dr.GetInt32(3) + "\r\n";
+                }
+                else
+                {
+                    textBox1.Text = "Nothing";
+                }
+
+            }
+            dr.Close();
+        }
+
+
+        //ЗАДАНИЕ 4     Показать овощи и фрукты с калорийностью в указанном диапазоне
+        // НЕ РАБОТАЕТ, ВЫДАЕТ ОШИБКУ 
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select * from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.GetInt32(3) > int.Parse(textBox3.Text) && dr.GetInt32(3) < int.Parse(textBox4.Text))
+                {
+                    textBox1.Text += dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2) + "\t" + dr.GetInt32(3) + "\r\n";
+                }
+
+            }
+            dr.Close();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select * from Vegetables_fruts";
+            cmd.Connection = conn;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (dr.GetString(2).ToLower() == "yellow" || dr.GetString(2).ToLower() == "red")
+                {
+                    textBox1.Text += dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2) + "\t" + dr.GetInt32(3) + "\r\n";
+                }
+
+            }
+            dr.Close();
+        }
+    }
+}
